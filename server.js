0efc2d7e32e013ca13f00c908918e4127fa58949
stupidCoder-arunsks <arunsks57@gmail.com>
2022-03-12 19:27:13 +0530

    completed response and request

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-  console.log("arunsks");
+  console.log(req.url,req.method,req.headers);
+  const reqUrl = req.url;
+// console.log('request Url >>>>>>> ' ,reqUrl === '/test' ? true : false);   
+    res.setHeader('Content-Type' , 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Hello from node js server...</title></head>');
+    if(req.url == '/home')
+    res.write('<body><h1>Welcome to my home</h1></body>');
+    if(req.url == '/about')
+    res.write('<body><h1>Welcome to about page</h1></body>');
+    if(req.url == '/node')
+    res.write('<body><h1>Welcome to my node Js project</h1></body>');
+    res.write('</html>');
+
 });
 
-server.listen(000);
+server.listen(3000);
