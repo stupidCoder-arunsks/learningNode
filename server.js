587688ab074747bbfecbe0429c06844d9437e400
stupidCoder-arunsks <arunsks57@gmail.com>
2022-03-15 20:37:37 +0530

    task 3 completed from express js

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit value="submit">Add Product</button></form>');
-});
+app.use('/admin' ,adminRoutes);
+app.use('/shop',shopRoutes);
 
-app.post('/product',(req,res,next) => {
-  console.log(req.body);
-   res.redirect('/');
-});
-
-app.use('/',(req,res,next) => {
-    res.send('<h1> Hello from Express! </h1>');
+app.use((req,res,next) => {
+ res.status(404).send('<h1> Page Not Found </h1>');
 });
 
 app.listen(3000);
