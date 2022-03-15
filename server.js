cf972b758db20669e1afcfbade3656dd65510dcb
stupidCoder-arunsks <arunsks57@gmail.com>
2022-03-15 18:29:47 +0530

    completed task 1 lets get started with express

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const routes = require('./routes.js');
- 
-const server = http.createServer(routes.handler);
+const express = require('express');
 
-server.listen(3000);
+const app = express();
+
+app.use((req,res,next) => {
+    console.log("In the middleWare...");
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log("In the another middleWare...");
+    // res.send('<h1> Hello from Express! </h1>');
+    res.send({key1:10});
+
+});
+
+app.listen(3000);
+ 
